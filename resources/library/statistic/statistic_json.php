4f03338162f3f949b51383f46ead54088fe9c923
Willy <admin@example.com>
2015-07-26 13:33:22 +0200

    Statistik überarbeitet (Zeigt letzten 7 Tage)

--- resources/library/statistic/statistic_json.php
+++ resources/library/statistic/statistic_json.php
@@ -17,14 +17,14 @@ switch ($_GET['type'])
 	$arr = array();
 	$arr['cols'][] = array('id' => '', 'label' => 'Zeit', 'type' => 'datetime');
 	$arr['cols'][] = array('id' => '', 'label' => 'Temperatur', 'type' => 'number');
-	
+
 	$lastTime = NULL;
 	$avg = array();
 	foreach ($log->getAll() as $row)
 	{
 		if ($lastTime !== NULL && $lastTime > $row[0])
 			continue;
-		
+
 		if ($lastTime !== NULL && $lastTime+400 < $row[0])
 		{
 			$skipped = round(($row[0] - $lastTime)/300);
@@ -44,26 +44,27 @@ switch ($_GET['type'])
 					array('v' => floatval(str_replace(array("\n", ','), array('', '.'), $row[1])))
 				);
 		}
-		
+
 		$lastTime = $row[0];
 	}
-	
+
+	$arr['rows'] = array_slice($arr['rows'], -2016);
 	$arr['max'] = round(max($avg) * 1.05);
 	$arr['min'] = round(min($avg) * 0.95);
 		break;
-		
+
 	case 'cpuload': // CPU-Auslastung
 	$arr = array();
 	$arr['cols'][] = array('id' => '', 'label' => 'Zeit', 'type' => 'datetime');
 	$arr['cols'][] = array('id' => '', 'label' => 'Auslastung', 'type' => 'number');
-	
+
 	$lastTime = NULL;
 	$avg = array();
 	foreach ($log->getAll() as $row)
 	{
 		if ($lastTime !== NULL && $lastTime > $row[0])
 			continue;
-		
+
 		if ($lastTime !== NULL && $lastTime+400 < $row[0])
 		{
 			$skipped = round(($row[0] - $lastTime)/300);
@@ -83,27 +84,28 @@ switch ($_GET['type'])
 					array('v' => floatval(str_replace(array("\n", ','), array('', '.'), $row[1])))
 				);
 		}
-		
+
 		$lastTime = $row[0];
 	}
-	
-	$arr['max'] = round(max($avg) * 1.05);
-	$arr['min'] = round(min($avg) * 0.95);
+
+	$arr['rows'] = array_slice($arr['rows'], -2016);
+	$arr['max'] = 100;
+	$arr['min'] = 0;
 		break;
-		
+
 	case 'ram': // Arbeitsspeicher
 	$arr = array();
 	$arr['cols'][] = array('id' => '', 'label' => 'Zeit', 'type' => 'datetime');
 	$arr['cols'][] = array('id' => '', 'label' => 'Auslastung', 'type' => 'number');
-	
+
 	$lastTime = NULL;
 	$avg = array();
 	foreach ($log->getAll() as $row)
 	{
 		if ($lastTime !== NULL && $lastTime > $row[0])
 			continue;
-		
-		
+
+
 		if ($lastTime !== NULL && $lastTime+400 < $row[0])
 		{
 			$skipped = round(($row[0] - $lastTime)/300);
@@ -123,20 +125,21 @@ switch ($_GET['type'])
 					array('v' => floatval(str_replace(array("\n", ','), array('', '.'), $row[1])))
 				);
 		}
-		
+
 		$lastTime = $row[0];
 	}
-	
-	$arr['max'] = round(max($avg) * 1.05);
-	$arr['min'] = round(min($avg) * 0.95);
+
+	$arr['rows'] = array_slice($arr['rows'], -2016);
+	$arr['max'] = 100;
+	$arr['min'] = 0;
 		break;
-	
+
 	case 'network': // Netzwerk
 	$arr = array();
 	$arr['cols'][] = array('id' => '', 'label' => 'Zeit', 'type' => 'datetime');
 	$arr['cols'][] = array('id' => '', 'label' => 'Gesendet', 'type' => 'number');
 	$arr['cols'][] = array('id' => '', 'label' => 'Empfangen', 'type' => 'number');
-	
+
 	$lastTime = NULL;
 	$avgUp = array();
 	$avgDown = array();
@@ -144,8 +147,8 @@ switch ($_GET['type'])
 	{
 		if ($lastTime !== NULL && $lastTime > $row[0])
 			continue;
-		
-		
+
+
 		if ($lastTime !== NULL && $lastTime+400 < $row[0])
 		{
 			$skipped = round(($row[0] - $lastTime)/300);
@@ -168,10 +171,11 @@ switch ($_GET['type'])
 					array('v' => floatval(str_replace(array("\n", ','), array('', '.'), round($row[2]/1048576,2))))
 				);
 		}
-		
+
 		$lastTime = $row[0];
 	}
-	
+
+	$arr['rows'] = array_slice($arr['rows'], -2016);
 	$arr['max'] = round(((max($avgUp) > max($avgDown)) ? max($avgUp) : max($avgDown)) * 1.10);
 	$arr['min'] = round(((min($avgUp) > min($avgDown)) ? min($avgDown) : min($avgUp)) * 0.90);
 		break;
