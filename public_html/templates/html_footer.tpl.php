8f18614aa29cd1103a6a1c24db62f3c98e5447cd
Willy <gitlab@willy-tech.de>
2015-10-25 22:54:20 +0100

    Neues Design

--- public_html/templates/html_footer.tpl.php
+++ public_html/templates/html_footer.tpl.php
@@ -1,19 +1,29 @@
 </div>
 <!-- Footer -->
 	<div id="footer">
-		<div id="footer-navi">
-			<a href="?s=settings"><?php _e('Einstellungen'); ?></a>
-			<a href="http://willy-tech.de/kontakt/" target="_blank"><?php _e('Feedback'); ?></a>
-			<a href="https://play.google.com/store/apps/details?id=de.willytech.picontrol" target="_blank" title="<?php _e('Öffne Play Store'); ?>"><?php _e('App im Play Store'); ?></a>
-			<a href="#" target="_blank"><?php _e('Hilfe'); ?></a>
-		</div>
-		<div id="footer-time">
-			<span><?php echo $data['servertime']; ?></span>
-		</div>
-		<div id="footer-copyright">
-			<p><?php _e('Version %s', $data['version']); ?></p>
-			<p><?php _e('Entwickelt von'); ?> <a href="http://willy-tech.de/" target="_blank">Willy Fritzsche</a>. 2013-2015</a></p>
-			<p><?php _e('Das Raspberry Pi Logo steht unter der Lizenz von <a href="http://www.raspberrypi.org/" target="_blank">www.raspberrypi.org</a>'); ?></p>
+		<div id="footer-inner">
+			<table id="footer-table">
+				<tr>
+					<th>PI CONTROL</th>
+					<th>ÜBER MICH</th>
+					<th>VERSION</th>
+				</tr>
+				<tr>
+					<td rowspan="2"><a href="?s=settings"><?php _e('Einstellungen'); ?></a><br />
+						<a href="http://willy-tech.de/kontakt/" target="_blank"><?php _e('Feedback'); ?></a><br />
+						<a href="#" target="_blank"><?php _e('Hilfe'); ?></a><br />
+						<a href="https://play.google.com/store/apps/details?id=de.willytech.picontrol" target="_blank" title="<?php _e('Öffne Play Store'); ?>"><?php _e('App im Play Store'); ?></a></td>
+					<td rowspan="2"><a href="#">Mein Blog</a><br />
+						<a href="#">Twitter</a><br />
+						<a href="#">GitHub</a><br />
+						<a href="#">Spenden</a></td>
+					<td>2.0.0</td>
+				</tr>
+				<tr>
+					<td><strong>LIZENZ</strong><span>Raspberry Pi ist eine Trademark<br />der <a href="http://www.raspberrypi.org/" target="_blank">Raspberry Pi Foundation</a>.</span></td>
+				</tr>
+			</table>
+			<div id="footer-copyright">Mit <span style="color: #F44336;">&#10084;</span> <?php _e('entwickelt von'); ?> <a href="http://willy-tech.de/" target="_blank">Willy Fritzsche</a>. 2013-2016</div>
 		</div>
 	</div>
 	<script type="text/javascript">var errorHandler = '<?php echo $data['errorHandler']; ?>';</script>
