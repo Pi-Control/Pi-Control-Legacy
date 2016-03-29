eb21ae9d7becfeed844ecd57578dce2d4ad693e8
Willy <gitlab@willy-tech.de>
2016-03-29 14:30:58 +0200

    Anmeldung über Token eingebaut

--- resources/library/main/authentification.php
+++ resources/library/main/authentification.php
@@ -2,13 +2,13 @@
 if (!defined('PICONTROL')) exit();
 
 (include_once realpath(dirname(__FILE__)).'/../../init.php')	or die('Fehler beim Laden der Seite. Konnte Konfigurationen nicht laden. Fehlercode: 0x0000');
-(include_once LIBRARY_PATH.'/main/main.function.php')			or die('Fehler beim Laden!');
+(include_once LIBRARY_PATH.'main/main.function.php')			or die('Fehler beim Laden!');
 
 $logout = true;
 
-if (isset($_COOKIE['_pi-control_login']))
+if (isset($_COOKIE['_pi-control_login']) || isset($_POST['token']))
 {
-	$uniqid = $_COOKIE['_pi-control_login'];
+	$uniqid = isset($_COOKIE['_pi-control_login']) ? $_COOKIE['_pi-control_login'] : $_POST['token'];
 	$tokenCreated = getConfig('login:token_'.$uniqid.'.created', 0);
 	$tokenRememberMe = getConfig('login:token_'.$uniqid.'.remember_me', 'false');
 	$tokenUsername = getConfig('login:token_'.$uniqid.'.username', '');
@@ -17,7 +17,9 @@ if (isset($_COOKIE['_pi-control_login']))
 	if ($tokenCreated == 0 || ($tokenCreated < time()-60*60*12 && $tokenRememberMe != 'true'))
 	{
 		removeConfig('login:token_'.$uniqid);
-		setcookie('_pi-control_login', '', time()-60);
+		
+		if (isset($_COOKIE['_pi-control_login']))
+			setcookie('_pi-control_login', '', time()-60);
 	}
 	elseif ($tokenLastLogin < time()-60*60)
 	{
@@ -28,7 +30,22 @@ if (isset($_COOKIE['_pi-control_login']))
 		$logout = false;
 }
 
-if (!isset($_COOKIE['_pi-control_login']) || $logout === true)
+if (isset($_POST['token']) && $logout === true)
+{
+	if (isset($authentificationMsg))
+		die($authentificationMsg);
+	else
+	{
+		$referer = isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : '';
+		
+		if ($referer != '')
+			$referer = '&referer='.urlencode($referer);
+		
+		header('HTTP/1.0 403 Forbidden');
+		exit();
+	}
+}
+elseif ((!isset($_COOKIE['_pi-control_login']) || $logout === true) && !isset($_POST['token']))
 {
 	if (isset($authentificationMsg))
 		die($authentificationMsg);
