e2ed738cffc5dc2a9b65c755a3d50943b1e916e4
Willy <gitlab@willy-tech.de>
2016-03-30 18:07:27 +0200

    Entfernen von nicht gebrauchten Dateien

--- resources/config/config_ssh.php
+++
@@ -1,5 +0,0 @@
-<?php
-$config_ssh_port = 22;
-$config_ssh_username = 'root';
-$config_ssh_password = 'Ax27Pz09NcST+rl5lsKm7I8BY3VoadSU03aadbGjM+4=';
-?>
--- resources/config/config_uniqid.php
+++
@@ -1,3 +0,0 @@
-<?php
-$config_uniqid = NULL; // 63af18dc1691f43c697520d04baea456
-?>
