656df841c85067c1a7e0147a64b2302796d726b2
Willy <gitlab@willy-tech.de>
2016-03-31 20:55:53 +0200

    Testplugin entfernt

--- resources/plugins/test/resources/content/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-$tpl->draw('index');
-?>
--- resources/plugins/test/resources/languages/en.php
+++
@@ -1,9 +0,0 @@
-<?php
-// English
-$langArray = array(
-	'0cbc6611' => 'TestENG',
-	'f990406a' => 'Only a Test-Plugin',
-	'799375c9' => 'Settings for Test',
-	'2ce867e3' => 'This is a Test-Plugin for a better understanding of the structure.'
-);
-?>
