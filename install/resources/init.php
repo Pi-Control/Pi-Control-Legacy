26334409cf2e2638c2620422aee0fdfd0b1954c9
Willy <gitlab@willy-tech.de>
2016-04-01 14:42:41 +0200

    Version angepasst

--- install/resources/init.php
+++ install/resources/init.php
@@ -8,8 +8,8 @@ if (!isset($config))
 			'ssh_ip'							=> '127.0.0.1'
 		),
 		'version' => array(
-			'version'							=> '2.0.0 Beta',
-			'versioncode'						=> 17,
+			'version'							=> '2.0.1 Beta',
+			'versioncode'						=> 18,
 			'android_comp_level'				=> 5
 		),
 		'url' => array(
