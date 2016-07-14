ab2620c4a5235f3fcb336ce0aeb43a1315f2fa2c
Willy <gitlab@willy-tech.de>
2016-07-14 16:38:19 +0200

    Übersetzung hinzugefügt

--- resources/content/settings/plugins_status.php
+++ resources/content/settings/plugins_status.php
@@ -8,13 +8,13 @@ if ($plugin['disabled'] == true)
 	if (setPluginStatus($plugin['id'], true) == true)
 		$tpl->msg('success', _t('Plugin aktiviert'), _t('Das Plugin "%s" wurde erfolgreich aktiviert.', $plugin['name']));
 	else
-		$tpl->msg('error', _t(''), _t('Das Plugin "%s" konnte nicht aktiviert werden.', $plugin['name']));
+		$tpl->msg('error', _t('Fehler'), _t('Das Plugin "%s" konnte nicht aktiviert werden.', $plugin['name']));
 }
 elseif ($plugin['disabled'] == false)
 {
 	if (setPluginStatus($plugin['id'], false) == true)
 		$tpl->msg('success', _t('Plugin deaktiviert'), _t('Das Plugin "%s" wurde erfolgreich deaktiviert.', $plugin['name']));
 	else
-		$tpl->msg('error', _t(''), _t('Das Plugin "%s" konnte nicht deaktiviert werden.', $plugin['name']));
+		$tpl->msg('error', _t('Fehler'), _t('Das Plugin "%s" konnte nicht deaktiviert werden.', $plugin['name']));
 }
 ?>
