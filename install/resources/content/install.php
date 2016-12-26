b6a92715b325cbc54ddc08364263c07ea5937110
Willy <gitlab@willy-tech.de>
2016-12-27 00:43:48 +0100

    Anforderungen entfernt

--- install/resources/content/install.php
+++ install/resources/content/install.php
@@ -3,8 +3,6 @@ if (!defined('PICONTROL')) exit();
 
 $tpl->setHeaderTitle(_t('Vorwort'));
 
-$tpl->msg('error', _t('Aktuell bekannte Einschr&auml;nkungen'), _t('- Das Terminal funktioniert nur unter HTTP, nicht HTTPS.'));
-
 $tpl->assign('langUrl', (isset($_GET['lang']) && $_GET['lang'] != '') ? '&amp;lang='.$_GET['lang'] : '');
 
 $tpl->draw('install');
