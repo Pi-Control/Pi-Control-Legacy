1944fc48a4d5ca9c09966bf66511ded782f412ac
Willy <gitlab@willy-tech.de>
2017-01-07 18:44:52 +0100

    Update von 1.3.3 auf 2.0 entfernt

--- update1.3.3-2.0/picontrol2-0/public_html/js/main.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).on('click', '.error > div span.cancel, .success > div span.cancel, .info > div span.cancel', function(e) {
-	jQuery(this).parents('.box').css("position", "relative").css("z-index", "-500").animate({marginTop: -jQuery(this).parents('.box').height()-20, opacity: 0}, 300, function(e)
-	{
-		jQuery(this).hide();
-	});
-});
