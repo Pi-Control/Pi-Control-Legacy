485a641f3855f4cc9168de2d617eb06760dbfda7
Willy <gitlab@willy-tech.de>
2017-02-17 16:13:02 +0100

    Wetter erweitert

--- public_html/templates/settings/overview.tpl.php
+++ public_html/templates/settings/overview.tpl.php
@@ -1,4 +1,5 @@
 <?php if (!defined('PICONTROL')) exit(); ?>
+<script type="text/javascript" src="public_html/js/settings.overview.weather.js"></script>
 <div>
 	<div class="box">
 		<div class="inner-header">
@@ -36,11 +37,18 @@
 					</tr>
 					<tr>
 						<td><?php _e('Dienst'); ?></td>
-						<td><select name="weather-service" onchange="if (this.selectedIndex == 0) jQuery('input[name=weather-service-token]').show(); else jQuery('input[name=weather-service-token]').hide();">
-								<option value="openweathermap"<?php if ($data['weather-service'] == 'openweathermap') echo ' selected="selected"'; ?>>OpenWeatherMap</option>
-								<option value="yahoo"<?php if ($data['weather-service'] == 'yahoo') echo ' selected="selected"'; ?>>Yahoo Weather</option>
+						<td><select name="weather-service">
+								<optgroup label="<?php _e('Ohne API-Key'); ?>">
+									<option value="yahoo"<?php if ($data['weather-service'] == 'yahoo') echo ' selected="selected"'; ?> data-url="https://www.yahoo.com/news/weather">Yahoo Weather</option>
+									<option value="yr"<?php if ($data['weather-service'] == 'yr') echo ' selected="selected"'; ?> data-url="https://www.yr.no/">Yr</option>
+								</optgroup>
+								<optgroup label="<?php _e('Mit API-Key'); ?>">
+									<option value="darksky"<?php if ($data['weather-service'] == 'darksky') echo ' selected="selected"'; ?> data-url="https://darksky.net/">Dark Sky</option>
+									<option value="openweathermap"<?php if ($data['weather-service'] == 'openweathermap') echo ' selected="selected"'; ?> data-url="https://openweathermap.org/">OpenWeatherMap</option>
+									<option value="wunderground"<?php if ($data['weather-service'] == 'wunderground') echo ' selected="selected"'; ?> data-url="https://www.wunderground.com/">Wunderground</option>
+								</optgroup>
 							</select></td>
-						<td><input type="text" name="weather-service-token" value="<?php echo $data['weather-service-token']; ?>" placeholder="API-Key" maxlength="32"<?php if ($data['weather-service'] == 'yahoo') echo ' style="display: none;"'; ?> /></td>
+						<td><input type="text" name="weather-service-token" value="<?php echo $data['weather-service-token']; ?>" placeholder="API-Key" maxlength="32"<?php if ($data['weather-service'] == 'yahoo') echo ' style="display: none;"'; ?> /> <a href="#" class="weather-service-link button" target="_blank">Zum Anbieter</a></td>
 					</tr>
 					<tr>
 						<td><?php _e('Standort'); ?></td>
@@ -52,15 +60,21 @@
 								<option value="uk"<?php if ($data['weather-country'] == 'uk') echo ' selected="selected"'; ?>><?php _e('Großbritannien'); ?></option>
 							</select> <?php if (is_array($data['weather-info'])) echo $data['weather-info']['city'].', '.$data['weather-info']['country']; elseif ($data['weather-info'] == 1) echo '<strong class="red">'._t('Server nicht erreichbar!').'</strong>'; ?></td>
 					</tr>
-					<tr>
+					<tr class="weather-location-type-postcode">
 						<td></td>
 						<td><input type="radio" name="weather-location-type" id="cb-weather-location-postcode" value="postcode"<?php if ($data['weather-type'] == 'postcode') echo ' checked="checked"'; ?> /><label for="cb-weather-location-postcode" class="radio"><?php _e('Postleitzahl'); ?></label></td>
 						<td><input type="text" name="weather-location-postcode-text" value="<?php echo $data['weather-postcode']; ?>" /></td>
 					</tr>
-					<tr>
+					<tr class="weather-location-type-city">
 						<td></td>
 						<td><input type="radio" name="weather-location-type" id="cb-weather-location-city" value="city"<?php if ($data['weather-type'] == 'city') echo ' checked="checked"'; ?> /><label for="cb-weather-location-city" class="radio"><?php _e('Stadt'); ?></label></td>
 						<td><input type="text" name="weather-location-city-text" value="<?php echo $data['weather-city']; ?>" /></td>
+					</tr>
+					<tr class="weather-location-type-coordinates">
+						<td></td>
+						<td><input type="radio" name="weather-location-type" id="cb-weather-location-coordinates" value="coordinates"<?php if ($data['weather-type'] == 'coordinates') echo ' checked="checked"'; ?> /><label for="cb-weather-location-coordinates" class="radio"><?php _e('Koordinaten'); ?></label></td>
+						<td>Breitengrad <input type="text" name="weather-location-coordinates-latitude-text" value="<?php echo $data['weather-coordinates-latitude']; ?>" /><br />
+							L&auml;ngengrad <input type="text" name="weather-location-coordinates-longitude-text" value="<?php echo $data['weather-coordinates-longitude']; ?>" /></td>
 					</tr>
 				</table>
 			</div>
--- resources/content/settings/overview.php
+++ resources/content/settings/overview.php
@@ -21,15 +21,19 @@ if (isset($_POST['submit-main']) && $_POST['submit-main'] != '')
 
 if (isset($_POST['submit-weather']) && $_POST['submit-weather'] != '')
 {
-	if (isset($_POST['weather-service'], $_POST['weather-location-country'], $_POST['weather-location-type']) && in_array($_POST['weather-service'], array('openweathermap', 'yahoo')) && in_array($_POST['weather-location-country'], array('germany', 'austria', 'swiss', 'uk'))  && ((isset($_POST['weather-service-token']) && $_POST['weather-service'] == 'openweathermap' && ($pWeatherServiceToken = trim($_POST['weather-service-token'])) != '') || ($_POST['weather-service'] == 'yahoo')) && (($_POST['weather-location-type'] == 'postcode' && isset($_POST['weather-location-postcode-text']) && $_POST['weather-location-postcode-text'] != '') || ($_POST['weather-location-type'] == 'city' && isset($_POST['weather-location-city-text']) && ($cityText = trim($_POST['weather-location-city-text'])) != '')))
+	if (isset($_POST['weather-service'], $_POST['weather-location-country'], $_POST['weather-location-type']) &&
+		in_array($_POST['weather-service'], array('openweathermap', 'yahoo', 'wunderground', 'darksky', 'yr')) &&
+		in_array($_POST['weather-location-country'], array('germany', 'austria', 'swiss', 'uk')) &&
+		((isset($_POST['weather-service-token']) && in_array($_POST['weather-service'], array('openweathermap', 'wunderground', 'darksky')) && ($pWeatherServiceToken = trim($_POST['weather-service-token'])) != '') || in_array($_POST['weather-service'], array('yahoo', 'yr'))) &&
+		(($_POST['weather-location-type'] == 'postcode' && isset($_POST['weather-location-postcode-text']) && $_POST['weather-location-postcode-text'] != '') || ($_POST['weather-location-type'] == 'city' && isset($_POST['weather-location-city-text']) && ($cityText = trim($_POST['weather-location-city-text'])) != '') || ($_POST['weather-location-type'] == 'coordinates' && isset($_POST['weather-location-coordinates-latitude-text'], $_POST['weather-location-coordinates-longitude-text']) && ($coordinatesLatitudeText = trim($_POST['weather-location-coordinates-latitude-text'])) != '' && ($coordinatesLongitudeText = trim($_POST['weather-location-coordinates-longitude-text'])) != '')))
 	{
 		setConfig('main:weather.service', $_POST['weather-service']);
 		setConfig('main:weather.country', $_POST['weather-location-country']);
 		setConfig('main:weather.type', $_POST['weather-location-type']);
 		
-		if (isset($pWeatherServiceToken) && strlen($pWeatherServiceToken) == 32 && $_POST['weather-service'] == 'openweathermap')
+		if (isset($pWeatherServiceToken) && ((strlen($pWeatherServiceToken) == 32 && in_array($_POST['weather-service'], array('openweathermap', 'darksky'))) || (strlen($pWeatherServiceToken) == 16 && in_array($_POST['weather-service'], array('wunderground')))))
 			setConfig('main:weather.serviceToken', $pWeatherServiceToken);
-		elseif ($_POST['weather-service'] == 'openweathermap')
+		elseif (in_array($_POST['weather-service'], array('openweathermap', 'wunderground', 'darksky')))
 			$tpl->msg('error', _t('Fehler'), _t('Leider ist der angegebene API-Schl&uuml;ssel zu kurz!'), true, 10);
 		else
 			setConfig('main:weather.serviceToken', '');
@@ -53,15 +57,32 @@ if (isset($_POST['submit-weather']) && $_POST['submit-weather'] != '')
 		}
 		elseif ($_POST['weather-location-type'] == 'city')
 		{
-			if (strlen($cityText) >= 3 && preg_match('/^[A-Za-zÄÖÜäöü \(\)\.]+$/', $cityText))
+			if (strlen($cityText) >= 3 && preg_match('/^[A-Za-zÄÖÜäöü \(\)\.\-\/]+$/', $cityText))
 				setConfig('main:weather.city', $cityText);
 			else
 				$tpl->msg('error', _t('Fehler'), _t('Leider ist der angegebene Stadtname ung&uuml;ltig!'), true, 10);
+		}
+		elseif ($_POST['weather-location-type'] == 'coordinates')
+		{
+			if (preg_match('/^-?[0-9]{1,2}([\.|,][0-9]{1,6})?$/', $coordinatesLatitudeText) && preg_match('/^-?[0-9]{1,2}([\.|,][0-9]{1,6})?$/', $coordinatesLongitudeText))
+			{
+				setConfig('main:weather.latitude', str_replace(',', '.', $coordinatesLatitudeText));
+				setConfig('main:weather.longitude', str_replace(',', '.', $coordinatesLongitudeText));
+			}
+			else
+				$tpl->msg('error', _t('Fehler'), _t('Leider sind die angegebenen Koordinaten ung&uuml;ltig!'), true, 10);
 		}
 		
 		if ($tpl->msgExists(10) === false)
 		{
 			setConfig('main:weather.activation', (isset($_POST['weather-activation']) && $_POST['weather-activation'] == 'checked') ? 'true' : 'false');
+			
+			if ($_POST['weather-service'] == 'yr')
+				setConfig('main:weather.yrCache', '');
+			
+			if ($_POST['weather-service'] == 'wunderground')
+				setConfig('main:weather.wundergroundCache', '');
+			
 			$tpl->msg('success', _t('Einstellungen gespeichert'), _t('Die Einstellungen wurden erfolgreich gespeichert.'));
 		}
 	}
@@ -78,6 +99,8 @@ $tpl->assign('weather-country', getConfig('main:weather.country', 'germany'));
 $tpl->assign('weather-type', getConfig('main:weather.type', 'postcode'));
 $tpl->assign('weather-city', getConfig('main:weather.city', ''));
 $tpl->assign('weather-postcode', getConfig('main:weather.postcode', ''));
+$tpl->assign('weather-coordinates-latitude', getConfig('main:weather.latitude', ''));
+$tpl->assign('weather-coordinates-longitude', getConfig('main:weather.longitude', ''));
 $tpl->assign('weather-info', getWeather());
 
 $tpl->draw('settings/overview');
